7c46b3244a688918e9aad1ee6879a4fd03424e1a
ADRIANO FROTA <adriano.internet@gmail.com>
2017-07-18 00:20:31 -0300

    consumindo opdc 1

---
+++ Scripts/js/Controllers/OpdcController.js
@@ -0,0 +1,22 @@
+﻿// Code goes here
+
+var app = angular.module('myapp', []);
+
+app.controller('OpdcController', function ($scope, $http) {
+
+
+    $http.get("http://opdct1.apphb.com/api/Categorias")
+        .then(onUserComplete, onError);
+
+    // Recebe o usuario
+    var onUserComplete = function (response) {
+        $scope.data = response.data;
+    };
+
+    var onError = function (reason) {
+        $scope.error = "ERRO AO TENTAR ACESSAR O SERVICO";
+    };
+
+
+});
+

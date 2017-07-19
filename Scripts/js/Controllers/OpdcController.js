33fd600d2dec8ea19e2fece19d43efabc79ef287
ADRIANO FROTA <adriano.internet@gmail.com>
2017-07-19 00:18:58 -0300

    TESTE CORS

--- Scripts/js/Controllers/OpdcController.js
+++ Scripts/js/Controllers/OpdcController.js
@@ -1,6 +1,7 @@
 ﻿// Code goes here
 
 var app = angular.module('myapp', []);
+ 
 
 app.controller('OpdcController', function ($scope, $http) {
 
@@ -8,13 +9,13 @@ app.controller('OpdcController', function ($scope, $http) {
     $http.get("http://opdct1.apphb.com/api/Categorias")
         .then(onUserComplete, onError);
 
-    // Recebe o usuario
     var onUserComplete = function (response) {
-        $scope.data = response.data;
+       $scope.list = response.data;
     };
 
     var onError = function (reason) {
         $scope.error = "ERRO AO TENTAR ACESSAR O SERVICO";
+       
     };
 
 
